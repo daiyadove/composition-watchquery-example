de36ab8ddc6f64b09e2249dbb8bd56bfffbfcd57
daiyadove <phaosdove@gmail.com>
2020-10-05 22:28:28 +0900

    ボタンをクリックするとQueryが変わる

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">composition-watchquery-example</h1>
+      <logo />
+      <h2 class="subtitle">Reactive Router query: {{ item_id }}</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
+        <a class="button--grey" @click="handleClick">
+          Change Router Query
         </a>
       </div>
     </div>
   </div>
 </template>
-
 <script lang="ts">
-import Vue from 'vue'
+import { computed, defineComponent, onMounted, watch } from '@vue/composition-api'
+import Logo from '@/components/Logo.vue'
+import useRouter from '@/use/useRouter'
+export default defineComponent({
+  components: {
+    Logo
+  },
+  setup() {
+    const { route, router } = useRouter()
+    const item_id = computed(() => route.value.query.item_id)
 
-export default Vue.extend({})
+    const handleClick = (e: Event) => {
+      e.preventDefault()
+      router.push({ query: { item_id: 'hoge' } })
+    }
+    return {
+      item_id,
+      handleClick
+    }
+  }
+})
 </script>
-
 <style>
 .container {
   margin: 0 auto;
@@ -40,17 +43,15 @@ export default Vue.extend({})
   align-items: center;
   text-align: center;
 }
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
-  color: #35495e;
+  color: #35495E;
   letter-spacing: 1px;
 }
-
 .subtitle {
   font-weight: 300;
   font-size: 42px;
@@ -58,7 +59,6 @@ export default Vue.extend({})
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-
 .links {
   padding-top: 15px;
 }
