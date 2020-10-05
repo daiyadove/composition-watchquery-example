c40cbb7f43115e389f9b376b0d161f821edeb961
daiyadove <phaosdove@gmail.com>
2020-10-05 22:24:36 +0900

    init

---
+++ use/useRouter.ts
@@ -0,0 +1,26 @@
+import { reactive, toRefs, watch } from '@vue/composition-api'
+import { Route } from 'vue-router'
+import { getRuntimeVM } from '~/utils/runtime'
+
+type State = {
+  route: Route
+}
+
+const useRouter = () => {
+  const vm = getRuntimeVM()
+  console.log(vm)
+  const state = reactive<State>({
+    route: vm.$route
+  })
+
+  watch(
+    () => vm.$route,
+    (r) => {
+      state.route = r as any
+    }
+  )
+
+  return { ...toRefs(state), router: vm.$router }
+}
+
+export default useRouter
